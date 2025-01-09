b9aa6f6e4349a1115bdb4844f0d2e4d9a375b9d3
Vyslon <santoni.thomas26@hotmail.fr>
2025-01-09 21:21:30 +0100

    création de la structure de hashtable

---
+++ GutenbergApp/management/commands/utils/indexing.py
@@ -0,0 +1,3 @@
+# TODO: en utilisant hash_table_indexing, on va ouvrir chaque livre 1 à 1,
+# compter le nombre d'occurences de chaque mots (il y a une fonction pour ça),
+# remplir petit à petit l'hash table et la sérialiser (elle sera déserialiser au moment de la recherche simple)
